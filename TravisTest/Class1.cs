438d1179824dd9fb8106b5dd7cea1523d184d7cb
blowfishpro <blowfishpro@yahoo.com>
2016-04-29 19:51:46 -0700

    A basic project

---
+++ TravisTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TravisTest
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class TravisTest : MonoBehaviour
+    {
+        public void Awake()
+        {
+            Debug.Log("Hello world!");
+        }
+    }
+}
